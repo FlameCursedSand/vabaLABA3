be41be91a3fd4b351e84cdd25baffb0bf3c34a7c
FlameCursedSand <157824725+FlameCursedSand@users.noreply.github.com>
2025-09-14 22:29:13 +0300

    Add files via upload

---
+++ ForTheLab3/TheLab3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double x, s;
+        Console.WriteLine("Введите конечное число");
+        x = Convert.ToDouble(Console.ReadLine());
+        s = 0;
+        for (double i = 0; i <= x; i++)
+        {
+            s += i;
+        }
+        Console.WriteLine(s);
+
+        }
+
+    } 
+
---
+++ WhileTheLab3/WhileTheLab3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double x, s;
+        Console.WriteLine("Введите конечное число");
+        x = Convert.ToDouble(Console.ReadLine());
+        s = 0;
+        double i = 0;
+        while (i <= x)
+        {
+            s += i;
+            i++;
+        }
+        Console.WriteLine(s);
+    }   
+}
